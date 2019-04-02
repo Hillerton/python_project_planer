3c3561a36323e3bb3778bfeeca403ced5cf3de4d
Hillerton <thomashillerton@gmail.com>
2019-04-02 15:13:47 +0200

    found some missing stuff

--- build_py_project.bash
+++ build_py_project.bash
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-# https://gist.github.com/robwierzbowski/5430952/
-# Create and push to a new github repo from the command line.  
-# Grabs sensible defaults from the containing folder and `.gitconfig`.  
-# Refinements welcome.
-
-# Gather constant vars
-CURRENTDIR=${PWD##*/}
-GITHUBUSER=$(git config github.user)
-
-# Get user input
-echo "Name for New repo"
-read REPONAME:-
-echo "Git Username"
-read USER
-echo "Repo Description:"
-each DESCRIPTION
 echo "Give name for python tool"
 read TOOL
+echo "Give path to repo to sync with"
+read path
 
 echo "Here we go..."
 
@@ -34,18 +20,18 @@ echo "#" $TOOL > README.md
 
 pip3 install -r $TOOL/requirements.txt
 
-# Curl some json to the github API oh damn we so fancy
-curl -u ${USER:-${GITHUBUSER}} https://api.github.com/user/repos -d "{\"name\": \"${REPONAME:-${CURRENTDIR}}\", \"description\": \"${DESCRIPTION}\", \"private\": false, \"has_issues\": true, \"has_downloads\": true, \"has_wiki\": false}"
+#initialise empty git
+git init
 
-# Set the freshly created repo to the origin and push
-# You'll need to have added your public key to your github account
-git remote set-url origin git@github.com:${USER:-${GITHUBUSER}}/${REPONAME:-${CURRENTDIR}}.git
-git push --set-upstream origin master
+pre-commit install 
+
+#add remote location 
+git remote add $path
 
 #add file structure to the new git 
 git add .
 git commit -m "first commit"
-git push
+git push 
 
 #add the pre-commit hooks to the git
 pre-commit install
